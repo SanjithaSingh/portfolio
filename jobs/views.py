4ede9ab32ab465ffc4bc1fa80f26bde3867cff04
Sanjitha Singh <sanjitha.singh@gmail.com>
2020-08-28 16:20:17 +0530

    added about section

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Job, Project
+from .models import Job, Project,Stack
 # Create your views here.
 def home(request):
     jobs = Job.objects
     projects= Project.objects
-    return render(request,'jobs/home.html',{'jobs':jobs,'projects':projects})
+    skills = Stack.objects
+    return render(request,'jobs/home.html',{'jobs':jobs,'projects':projects,'skills':skills})
 
 def detail(request, job_id):
     job_detail = get_object_or_404(Job,pk=job_id)
